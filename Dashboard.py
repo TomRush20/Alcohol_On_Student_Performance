b28ea2e2cda0fe5b899a7dc9616c8d2b671ca616
TomRush20 <rushtw24@wfu.edu>
2024-11-20 14:16:58 -0500

    Initial Commit

---
+++ Dashboard.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(layout='wide')
+
+st.header('Meet the Team')
+
+st.write('Charlie: Leader')
+st.write('Herman: Organizer')
+st.write('Tom: Design Lead')
+st.write('Patrick: Data Analyst')
+st.write('Christian: Model Developer/Test Subject/Groupie')
+
+st.image('shame-game-of-thrones.gif')
