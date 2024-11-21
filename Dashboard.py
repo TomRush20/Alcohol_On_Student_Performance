5743dde53ad8a38bb8e7559676a3d9a6ca218a55
Patrick <wpsweeney5@gmail.com>
2024-11-21 09:30:31 -0500

    added nav template

--- Dashboard.py
+++ Dashboard.py
@@ -3,14 +3,27 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-st.set_page_config(layout='wide')
+st.set_page_config(layout='wide',initial_sidebar_state="collapsed")
 
-st.header('Meet the Team')
+def intro():
+    st.title('Meet the Team')
 
-st.write('Charlie: Leader')
-st.write('Herman: Organizer')
-st.write('Tom: Design Lead')
-st.write('Patrick: Data Analyst')
-st.write('Christian: Model Developer/Test Subject/Groupie')
+    st.write('Charlie: Leader')
+    st.write('Herman: Organizer')
+    st.write('Tom: Design Lead')
+    st.write('Patrick: Data Analyst')
+    st.write('Christian: Model Developer/Test Subject/Groupie')
 
-st.image('shame-game-of-thrones.gif')
+    st.image('shame-game-of-thrones.gif')
+
+
+
+def data():
+    st.title('Data Overview')
+
+
+
+st.balloons()
+
+nav = st.navigation([st.Page(intro,title='Meet the Team'), st.Page(data,title='Data Overview')])
+nav.run()
